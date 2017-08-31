cbeb9941e906d1d3e48cb7914af9eb7c4b26fba5
Lovepreet <lovepreet.singh@debutinfotech.com>
2017-08-31 15:14:09 +0530

    Changes

--- project/resources/views/admin/dashboard.blade.php
+++ project/resources/views/admin/dashboard.blade.php
@@ -13,7 +13,7 @@ Dashboard
 			<!-- BEGIN DASHBOARD STATS -->
 			<div class="row">
 				<div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
-				<a href="{{ url(getenv('adminurl').'/tasks') }}">
+				<a href="{{ url(getenv('adminurl').'/users') }}">
 					<div class="dashboard-stat blue-madison">
 						<div class="visual">
 							<i class="glyphicon glyphicon-screenshot"></i>
@@ -35,7 +35,8 @@ Dashboard
 
 
 				<div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
-				<a href="{{ url(getenv('adminurl').'/users') }}">
+				<a href="{{ url(getenv('adminurl').'/users/banned') }}">
+				<a href="javascript:void(0);"></a>
 					<div class="dashboard-stat red-intense">
 						<div class="visual">
 							<i class="glyphicon glyphicon-home"></i>
@@ -48,14 +49,15 @@ Dashboard
 								 Banned user
 							</div>
 						</div>
-						<a class="more" href="{{ url(getenv('adminurl').'/users') }}">
+						<a class="more" href="{{ url(getenv('adminurl').'/users/banned') }}">
 						View more <i class="m-icon-swapright m-icon-white"></i>
 						</a>
 					</div>
 						</a>
 				</div>
 				<div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
-				<a href="{{ url(getenv('adminurl').'/users') }}">
+				<!-- <a href="{{ url(getenv('adminurl').'/users') }}"> -->
+					<a href="javascript:void(0);"></a>
 					<div class="dashboard-stat green-haze">
 						<div class="visual">
 							<i class="glyphicon glyphicon-registration-mark"></i>
@@ -68,7 +70,8 @@ Dashboard
 								 Reports
 							</div>
 						</div>
-						<a class="more" href="{{ url(getenv('adminurl').'/users') }}">
+						<!-- <a class="more" href="{{ url(getenv('adminurl').'/users') }}"> -->
+						<a href="javascript:void(0);"></a>
 						View more <i class="m-icon-swapright m-icon-white"></i>
 						</a>
 					</div>
@@ -76,7 +79,8 @@ Dashboard
 				</div>
 				
 				<div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
-				<a href="{{ url(getenv('adminurl').'/properties') }}">
+				<!-- <a href="{{ url(getenv('adminurl').'/properties') }}"> -->
+					<a href="javascript:void(0);"></a>
 					<div class="dashboard-stat purple-plum">
 						<div class="visual">
 							<i class="fa fa-cube"></i>
@@ -89,7 +93,8 @@ Dashboard
 								 Photos
 							</div>
 						</div>
-						<a class="more" href="{{ url(getenv('adminurl').'/properties') }}">
+						<!-- <a class="more" href="{{ url(getenv('adminurl').'/properties') }}"> -->
+						<a href="javascript:void(0);"></a>
 						View more <i class="m-icon-swapright m-icon-white"></i>
 						</a>
 					</div>
@@ -97,7 +102,8 @@ Dashboard
 				</div>
 
 				<div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
-				<a href="{{ url(getenv('adminurl').'/users') }}">
+				<!-- <a href="{{ url(getenv('adminurl').'/users') }}"> -->
+					<a href="javascript:void(0);"></a>
 					<div class="dashboard-stat purple-plum">
 						<div class="visual">
 							<i class="fa fa-cube"></i>
@@ -110,7 +116,8 @@ Dashboard
 								 Profile Text
 							</div>
 						</div>
-						<a class="more" href="{{ url(getenv('adminurl').'/users') }}">
+						<!-- <a class="more" href="{{ url(getenv('adminurl').'/users') }}"> -->
+						<a href="javascript:void(0);"></a>
 						View more <i class="m-icon-swapright m-icon-white"></i>
 						</a>
 					</div>
