49ed2c6c30d761f8f288e41ad2a38bdc8865bcf4
Lovepreet <lovepreet.singh@debutinfotech.com>
2017-09-20 12:07:29 +0530

    Changes

--- js/path.js
+++ js/path.js
@@ -1,5 +1,6 @@
-path = "http://localhost/lookingApp/";
+//path = "http://localhost/lookingApp/";
 	//path = "http://frontline.debutinfotech.com/";
+	path = "http://lookingapp.debutinfotech.com/lookingapp_web/app/webroot/QA/";
 
 
 // this is admin name and has been used in many views in many scripts
