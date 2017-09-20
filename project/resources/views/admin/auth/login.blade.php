0fb124c7fe388ea5353e063a8ec48141e7924a2c
Lovepreet <lovepreet.singh@debutinfotech.com>
2017-09-20 12:30:43 +0530

    Changes

--- project/resources/views/admin/auth/login.blade.php
+++ project/resources/views/admin/auth/login.blade.php
@@ -117,7 +117,7 @@ License: You must have a valid license purchased only from themeforest(the above
 	
 </div>
 <div class="copyright">
-	 2016 © FrontlineApp.
+	 2017 © LookingApp.
 </div>
 <!-- END LOGIN -->
 <!-- BEGIN JAVASCRIPTS(Load javascripts at bottom, this will reduce page load time) -->
