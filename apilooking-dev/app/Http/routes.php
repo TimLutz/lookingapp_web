b035aa47a9e64b9ba62a65f056226426f82c1ef5
Lovepreet <lovepreet.singh@debutinfotech.com>
2017-10-17 11:09:35 +0530

    Changes

--- apilooking-dev/app/Http/routes.php
+++ apilooking-dev/app/Http/routes.php
@@ -68,7 +68,7 @@ $app->get('/', function () use ($app) {
     $app->post('add_recent_image','UserController@postAddRecentImage');
     $app->post('profile_viewers_details','UserController@getProfileViewersDetails');
     $app->post('profile_viewed_details','UserController@getProfileViewedDetails');
-    $app->get('use_profile_looksex','UserController@getUseProfileLooksex');
+    $app->post('use_profile_looksex','UserController@getUseProfileLooksex');
     
 });
 
