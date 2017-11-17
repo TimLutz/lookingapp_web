dfccef968540a591e9a0f25717c0630f19b6a716
Lovepreet <lovepreet.singh@debutinfotech.com>
2017-11-17 11:43:55 +0530

    Changes QA

--- looking/wp-config.php
+++ looking/wp-config.php
@@ -20,13 +20,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'lookingapp1');
+define('DB_NAME', 'looking_web');
 
 /** MySQL database username */
-define('DB_USER', 'lookingapp1');
+define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'Admin@2020');
+define('DB_PASSWORD', 'root');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
